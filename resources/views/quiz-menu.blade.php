1142bdd828aa6385b48942abd84cebcf0e80cd08
Otto Kuikka <otto.kuikka@kooditar.fi>
2023-04-10 18:25:57 +0300

    Use quiz-menu.blade as base for the quiz

--- resources/views/quiz-menu.blade.php
+++ resources/views/quiz-menu.blade.php
@@ -1,5 +1,12 @@
+<head>
+<livewire:styles>
+</head>
+<body>
 <div>
     <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
     <h1>What would you do in a situation like this?</h1>
     <h2>:message</h2>
+    <livewire:question />
+<livewire:scripts />
 </div>
+</body>
--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -1,14 +1,12 @@
-<!DOCTYPE html>
-<html>
 <head>
-    Answer the questions...
+<livewire:styles>
 </head>
 <body>
-    <div>
-        <x-Question 
-            :type="$type ?? 0"
-            :content="$content ?? ''"
-        />
-    </div>
+<div>
+    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
+    <h1>What would you do in a situation like this?</h1>
+    <h2>:messagessss</h2>
+    <livewire:question />
+<livewire:scripts />
+</div>
 </body>
-</html>
