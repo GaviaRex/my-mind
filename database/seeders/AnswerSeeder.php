0302ad45b7cf5b38d3452e3d97c86e83d5d7c3b1
Otto Kuikka <otto.kuikka@kooditar.fi>
2023-04-10 18:37:48 +0300

    Update AnswerSeeder

--- database/seeders/AnswerSeeder.php
+++ database/seeders/AnswerSeeder.php
@@ -14,6 +14,7 @@ class AnswerSeeder extends Seeder
      */
     public function run()
     {
+        // Answers for question 1
         DB::table('answers')->insert([
             'question_id' => 1,
             'type' => 1,
@@ -34,6 +35,56 @@ class AnswerSeeder extends Seeder
 
         DB::table('answers')->insert([
             'question_id' => 1,
+            'type' => 4,
+            'content' => "Answer 4"
+        ]);
+
+        // Answers for question 2
+        DB::table('answers')->insert([
+            'question_id' => 2,
+            'type' => 1,
+            'content' => "Answer 1"
+        ]);
+
+        DB::table('answers')->insert([
+            'question_id' => 2,
+            'type' => 2,
+            'content' => "Answer 2"
+        ]);
+
+        DB::table('answers')->insert([
+            'question_id' => 2,
+            'type' => 3,
+            'content' => "Answer 3"
+        ]);
+
+        DB::table('answers')->insert([
+            'question_id' => 2,
+            'type' => 4,
+            'content' => "Answer 4"
+        ]);
+
+        // Answers for question 3
+        DB::table('answers')->insert([
+            'question_id' => 3,
+            'type' => 1,
+            'content' => "Answer 1"
+        ]);
+
+        DB::table('answers')->insert([
+            'question_id' => 3,
+            'type' => 2,
+            'content' => "Answer 2"
+        ]);
+
+        DB::table('answers')->insert([
+            'question_id' => 3,
+            'type' => 3,
+            'content' => "Answer 3"
+        ]);
+
+        DB::table('answers')->insert([
+            'question_id' => 3,
             'type' => 4,
             'content' => "Answer 4"
         ]);
